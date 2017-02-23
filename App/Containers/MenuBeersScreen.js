2fc1540c277738851ddc611d9f98ee84cf7de47a
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-23 14:17:52 -0800

    [progress] Get all menu pages to render appropriate drinks

--- App/Containers/MenuBeersScreen.js
+++ App/Containers/MenuBeersScreen.js
@@ -2,18 +2,14 @@
 
 import React from 'react'
 import { ScrollView, View, Image } from 'react-native'
-import RNFetchBlob from 'react-native-fetch-blob'
 import { Metrics, Images } from '../Themes'
 import FullButton from '../Components/FullButton'
 
 // For API
 import API from '../Services/Api'
-import MenuConfig from '../Config/MenuConfig'
 
 // Styles
 import styles from './Styles/BarMenuScreenStyle'
-
-const DOMAIN = MenuConfig.domain
 
 export default class APITestingScreen extends React.Component {
   api: Object
@@ -30,15 +26,6 @@ export default class APITestingScreen extends React.Component {
     }
 
     this.api = API.create()
-  }
-
-  componentDidMount () {
-    RNFetchBlob.fetch('GET', `${DOMAIN}/drinks/getall/`)
-      .then(res => {
-        this.createBeerArr(res.json().beerArr)
-      }).catch(err => {
-        console.log('err', err)
-      })
   }
 
   createBeerArr (beers) {
@@ -64,7 +51,7 @@ export default class APITestingScreen extends React.Component {
       <View style={styles.blackContainer}>
         <ScrollView style={styles.container} ref='container'>
           <Image source={Images.barMockHeader} style={styles.menuHeaderImage} resizeMode='stretch' />
-          {this.state.beers.map(beer => <FullButton text={beer.name} key={beer.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
+          {this.props.beers.map(beer => <FullButton text={beer.name} key={beer.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
         </ScrollView>
       </View>
     )
