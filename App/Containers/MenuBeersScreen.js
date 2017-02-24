f2040a133dc61332d0b1996c66c9eba4e1c1d68f
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-23 23:09:48 -0800

    [refactor] OrderModal into own file and get modals working for cocktails/shots

--- App/Containers/MenuBeersScreen.js
+++ App/Containers/MenuBeersScreen.js
@@ -1,27 +1,27 @@
 // @flow
 
 import React from 'react'
-import { ScrollView, View, Image, TouchableOpacity, Text, Button } from 'react-native'
+import { ScrollView, View, Image, Button } from 'react-native'
 import { Metrics, Images } from '../Themes'
 import MenuFullButton from '../Components/MenuFullButton'
+import OrderModal from './MenuOrderModal'
 
 // Styles
 import styles from './Styles/MenuBarScreenStyle'
 
-export default class APITestingScreen extends React.Component {
+export default class MenuBeers extends React.Component {
   constructor (props: Object) {
     super(props)
     this.state = {
       visibleHeight: Metrics.screenHeight,
-      beers: [],
       renderModal: false,
       beerClicked: null
     }
 
-    this.beerClicked = this.beerClicked.bind(this)
+    this.displayBeerModal = this.displayBeerModal.bind(this)
   }
 
-  beerClicked (beer) {
+  displayBeerModal (beer) {
     this.setState({
       renderModal: true,
       beerClicked: beer
@@ -33,58 +33,11 @@ export default class APITestingScreen extends React.Component {
       <View style={styles.blackContainer}>
         <Image source={Images.barMockHeader} style={styles.menuHeaderImage} resizeMode='stretch' />
         <ScrollView style={styles.menuContainer} ref='container' scrollEnabled={false}>
-          {this.props.beers.map(beer => <MenuFullButton onClickedItem={this.beerClicked} item={beer} price={beer.price} text={beer.name} key={beer.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
+          {this.props.beers.map(beer => <MenuFullButton onClickedItem={this.displayBeerModal} item={beer} price={beer.price} text={beer.name} key={beer.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
         </ScrollView>
-        <Button title='Close Tab' onPress={() => { console.log('closing tab') }}>Close Tab</Button>
         {this.state.renderModal ? <OrderModal order={this.state.beerClicked} /> : <OrderModal />}
+        <Button title='Close Tab' onPress={() => { console.log('closing tab') }}>Close Tab</Button>
       </View>
     )
-  }
-
-}
-
-class OrderModal extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      order: null,
-      displayModal: true
-    }
-    this.renderModal = this.renderModal.bind(this)
-    this.removeModal = this.removeModal.bind(this)
-  }
-
-  removeModal () {
-    this.setState({
-      displayModal: false
-    })
-  }
-
-  componentDidUpdate (prevProps, prevState) {
-    if (!prevState.displayModal) {
-      this.setState({ displayModal: true })
-    }
-  }
-
-  renderModal () {
-    return (<ScrollView style={{ top: 0, bottom: 0, left: 0, right: 0, position: 'absolute' }} overflow='hidden'>
-      <TouchableOpacity
-        onPress={this.removeModal}
-        style={{backgroundColor: 'white', padding: 20}}
-      >
-        <Text>{JSON.stringify(this.props.order)}</Text>
-        <Text allowFontScaling={false} style={{fontFamily: 'CourierNewPS-BoldMT', fontSize: 10}}>
-          {this.state.message}
-        </Text>
-      </TouchableOpacity>
-    </ScrollView>)
-  }
-
-  render () {
-    if (this.props.order && this.state.displayModal) {
-      return this.renderModal(this.props.order)
-    }
-
-    return <Text />
   }
 }
--- App/Containers/MenuCocktailsScreen.js
+++ App/Containers/MenuCocktailsScreen.js
@@ -4,20 +4,27 @@ import React from 'react'
 import { ScrollView, View, Image, Button } from 'react-native'
 import { Metrics, Images } from '../Themes'
 import MenuFullButton from '../Components/MenuFullButton'
+import OrderModal from './MenuOrderModal'
 
 // Styles
 import styles from './Styles/MenuBarScreenStyle'
 
-export default class APITestingScreen extends React.Component {
+export default class MenuCocktail extends React.Component {
   constructor (props: Object) {
     super(props)
     this.state = {
-      visibleHeight: Metrics.screenHeight
+      visibleHeight: Metrics.screenHeight,
+      renderModal: false,
+      cocktailClicked: null
     }
+    this.displayCocktailModal = this.displayCocktailModal.bind(this)
   }
 
-  addCocktailToTab () {
-
+  displayCocktailModal (shot) {
+    this.setState({
+      renderModal: true,
+      cocktailClicked: shot
+    })
   }
 
   render () {
@@ -25,8 +32,9 @@ export default class APITestingScreen extends React.Component {
       <View style={styles.blackContainer}>
         <Image source={Images.barMockHeader} style={styles.menuHeaderImage} resizeMode='stretch' />
         <ScrollView style={styles.container} ref='container'>
-          {this.props.cocktails.map(cocktail => <MenuFullButton price={cocktail.price} text={cocktail.name} key={cocktail.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
+          {this.props.cocktails.map(cocktail => <MenuFullButton onClickedItem={this.displayCocktailModal} item={cocktail} price={cocktail.price} text={cocktail.name} key={cocktail.name} styles={{marginTop: 0, marginBottom: 0, backgroundColor: '#1A2930'}} />)}
         </ScrollView>
+        {this.state.renderModal ? <OrderModal order={this.state.cocktailClicked} /> : <OrderModal />}
         <Button title='Close Tab' onPress={() => { console.log('closing tab') }} />
       </View>
     )
