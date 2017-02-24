459c8534cf19b1f78f02517e5d238ff49f8fde11
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-23 22:48:07 -0800

    [progress] Get a modal to render when beer is pressed

--- App/Containers/Styles/MenuBarScreenStyle.js
+++ App/Containers/Styles/MenuBarScreenStyle.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { ApplicationStyles, Metrics, Colors } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
@@ -12,5 +12,12 @@ export default StyleSheet.create({
     bottom: 0,
     right: 0,
     height: 150
+  },
+  menuContainer: {
+    flex: 1,
+    paddingTop: Metrics.baseMargin
+  },
+  closeTabBtn: {
+    color: Colors.barambeYellow
   }
 })
