c7f1659f39ace4ba04885820a0030170b4335748
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-25 11:58:02 -0800

    [feat] User slides a drink to confirm order

--- App/Components/MenuFullButton.js
+++ App/Components/MenuFullButton.js
@@ -6,6 +6,10 @@ import {SlideButton, SlideDirection} from 'react-native-slide-button'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import styles from './Styles/MenuFullButtonStyle'
 import { Colors, Metrics } from '../Themes'
+import MenuConfig from '../Config/MenuConfig'
+
+const DOMAIN = MenuConfig.domain
+const TEMP_TAB_ID = '2'
 
 export default class FullButton extends React.Component {
   constructor (props) {
@@ -15,10 +19,11 @@ export default class FullButton extends React.Component {
     }
     this.itemClicked = this.itemClicked.bind(this)
     this.removeConfirmSlider = this.removeConfirmSlider.bind(this)
+    this.onSlide.bind(this)
+    this.orderDrink.bind(this)
   }
 
   itemClicked (item) {
-    console.log('itemClicked')
     if (this.props.price) {
       const obj = Object.assign(this.state, {})
       obj.hideSlider = false
@@ -28,13 +33,31 @@ export default class FullButton extends React.Component {
     }
   }
 
-  onSlide () {
-    console.log('onSlide invoked')
+  orderDrink (order) {
+    fetch(`${DOMAIN}/orders/addorder/`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        drinkName: order.name,
+        tabId: TEMP_TAB_ID
+      })
+    }).then(res => {
+      console.log('res', res)
+    }).catch(err => {
+      console.log('err', err)
+    })
+  }
+
+  onSlide (order) {
+    this.orderDrink(order)
+    this.removeConfirmSlider()
     this.setState({ hideSlider: true })
   }
 
   removeConfirmSlider () {
-    console.log('removeConfirmSlider')
     this.setState({ hideSlider: true })
   }
 
@@ -47,14 +70,14 @@ export default class FullButton extends React.Component {
         ? <Text style={styles.priceText}> {this.props.price} </Text>
         : this.state.hideSlider
         ? <Text style={styles.chevronRight}> <Icon name='angle-right' size={Metrics.icons.small} color={Colors.barambeGrey} /> </Text>
-        : <View style={{backgroundColor: Colors.barambeBlue, width: Metrics.screenWidth, height: 35}} onAccessibilityTap={this.onSlide.bind(this)}>
+        : <View style={{backgroundColor: Colors.barambeBlue, width: Metrics.screenWidth, height: 35}}>
           <TouchableOpacity onPress={this.removeConfirmSlider} >
             <Icon name='close' size={Metrics.icons.tiny} color={Colors.barambeGrey} />
           </TouchableOpacity>
           <SlideButton
             width={Metrics.screenWidth}
             height={50}
-            onSlideSuccess={this.onSlide.bind(this)}
+            onSlideSuccess={() => { this.onSlide(this.props.item) }}
             slideDirection={SlideDirection.RIGHT}
             style={{marginLeft: 50, marginVertical: -8}}
           >
