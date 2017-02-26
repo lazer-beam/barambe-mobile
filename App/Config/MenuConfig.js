6c237c07e75ce5ec988e254f0f9ae0bd72854862
Andrew Tran <avtran23@gmail.com>
2017-02-25 16:40:12 -0800

    [progress]Allow cardForm to send cardInfo

--- App/Config/MenuConfig.js
+++ App/Config/MenuConfig.js
@@ -1,5 +1,5 @@
 import DeviceInfo from 'react-native-device-info'
 
 export default {
-  domain: DeviceInfo.getManufacturer() === 'Genymotion' ? 'http://10.0.3.2:1337' : 'http://10.0.2.2:1337/'
+  domain: DeviceInfo.getManufacturer() === 'Genymotion' ? 'http://10.0.3.2:1337' : 'http://10.0.2.2:1337'
 }
