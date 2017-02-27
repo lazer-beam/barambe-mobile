dd2e45eaa05682833ba6263b0d28782287fab70b
Andrew Tran <avtran23@gmail.com>
2017-02-26 16:59:37 -0800

    [fix]Show alert on cardForm on faulty data format

--- App/Containers/BarLandingScreen.js
+++ App/Containers/BarLandingScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+/*import React, { Component } from 'react'
 import { ScrollView, TouchableOpacity, Text, Image, TextInput, View } from 'react-native'
 import FullButton from '../Components/FullButton'
 import styles from './Styles/BarLandingScreenStyle'
@@ -26,7 +26,7 @@ export default class BarScreen extends React.Component {
             left: 0,
             bottom: 0,
             right: 0}}
-            source={{uri:'https://www.placecage.com/300/200'}}
+            source={{uri:'http://img04.deviantart.net/4281/i/2010/010/f/6/paddy__s_pub_by_detroitchicago.jpg'}}
             resizeMode='stretch'/>
         <ScrollView style={styles.container}>
           <View style={styles.section}>
@@ -39,8 +39,8 @@ export default class BarScreen extends React.Component {
               Payment
             </Text>
             <Text>
-              {/*Normally insert this.props.cardNickname, this.props.Last4Digits*/}
-              Andrew Credit 7259
+              {/*Normally insert this.props.cardNickname, this.props.Last4Digits*/
+              /*Andrew Credit 7259
             </Text>
             <Text onPress={NavigationActions.creditCardScreen}>
               Change
@@ -48,6 +48,46 @@ export default class BarScreen extends React.Component {
           </View>          
         </ScrollView>
         <FullButton text='OPEN TAB' />
+      </View>
+    )
+  }
+}*/
+
+// @flow
+
+import React from 'react'
+import { ScrollView, View, Image, Button } from 'react-native'
+import RNFetchBlob from 'react-native-fetch-blob'
+import { Actions as NavigationActions } from 'react-native-router-flux'
+
+import { Metrics, Images } from '../Themes'
+import MenuFullButton from '../Components/MenuFullButton'
+import MenuConfig from '../Config/MenuConfig'
+
+// Styles
+import styles from './Styles/MenuBarScreenStyle'
+
+const DOMAIN = MenuConfig.domain
+
+export default class APITestingScreen extends React.Component {
+  constructor (props: Object) {
+    super(props)
+    this.state = {
+      visibleHeight: Metrics.screenHeight,
+      beers: [],
+      shots: [],
+      cocktails: [],
+      addIns: []
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.blackContainer}>
+        <ScrollView style={styles.container} ref='container'>
+          <Image source={{uri:'http://img04.deviantart.net/4281/i/2010/010/f/6/paddy__s_pub_by_detroitchicago.jpg'}} style={styles.menuHeaderImage} resizeMode='stretch' />
+        </ScrollView>
+        <Button title='Open Tab' onPress={() => { console.log('closing tab') }}></Button>
       </View>
     )
   }
